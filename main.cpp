9f8f902d570e9a355a489f1fc9e5a70d41669cd9
lemonhack27 <lemonhack27@gmail.com>
2023-02-21 17:20:02 +0800

    修改sort为快排

--- main.cpp
+++ main.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
 using namespace std;
 
+void sort(int arr[], int i, int j)
+{
+    if(i > j)
+        return;
+    int val = arr[i];
+    int tmp;
+    int l = i;
+    int r = j;
+    while(l < r)
+    {
+        while (l < r && arr[i] >= val)
+        {
+            r--;
+        }
+        if(l < r)
+        {
+            arr[l++] = arr[r];
+        }
+        while(l < r && arr[l] <= val)
+        {
+            l++;
+        }
+        if(l < r)
+        {
+            arr[r--] = arr[l];
+        }
+    }
+
+    arr[i] = arr[l];
+    arr[l] = val;
+    sort(arr, i, l-1);
+    sort(arr, l+1, j);
+}
+
 void sort(int arr[], int size)
 {
-    int tmp;
-    for(int i = 0; i < size; ++i)
-        for(int j = i; j < size; ++j)
-        {
-            if(arr[i] < arr[j])
-            {
-                tmp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tmp;
-            }
-        }
+    sort(arr, 0, size);
 }
 
 int main()
