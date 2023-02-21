daa01d68ec7ba664f080fa3059d4cfa62b276e55
lemonhack27 <lemonhack27@gmail.com>
2023-02-21 10:28:13 +0800

    创建main.cpp文件，写了初始代码“

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void sort(int arr[], int size)
+{
+    
+}
+
+int main()
+{
+    int arr[] = {12,4,5,9,7,3,9};
+    int size = sizeof(arr)/sizof(arr[0]);
+    sort(arr,size);
+    return 0;
+}
