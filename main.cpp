da8199deed34d9983195f93c9251a5d7b096b7ef
lemonhack27 <lemonhack27@gmail.com>
2023-02-21 16:04:05 +0800

    添加sort

--- main.cpp
+++ main.cpp
@@ -3,13 +3,27 @@ using namespace std;
 
 void sort(int arr[], int size)
 {
-    
+    int tmp;
+    for(int i = 0; i < size; ++i)
+        for(int j = i; j < size; ++j)
+        {
+            if(arr[i] < arr[j])
+            {
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
 }
 
 int main()
 {
     int arr[] = {12,4,5,9,7,3,9};
-    int size = sizeof(arr)/sizof(arr[0]);
+    int size = sizeof(arr)/sizeof(arr[0]);
     sort(arr,size);
+    for(int i = 0; i < size; ++i)
+    {
+        cout<<arr[i]<<endl;
+    }
     return 0;
 }
